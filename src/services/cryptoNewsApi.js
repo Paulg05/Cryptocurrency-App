5820f96afc1a9a7426026075111f17e3d3f944f2
Paul Gonzalez <robertgonzalez018@gmail.com>
2021-09-29 11:27:01 -0700

    aded: created NewsCryptoApi and query to make a call

---
+++ src/services/cryptoNewsApi.js
@@ -0,0 +1,22 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const cryptoNewsHeaders =  {
+  'x-bingapis-sdk': 'true',
+  'x-rapidapi-host': 'bing-news-search1.p.rapidapi.com',
+  'x-rapidapi-key': 'cbd49c280amsh357e0f10bd1fed8p1c7abajsnb602d80b00a7'
+}
+
+const baseUrl = 'https://bing-news-search1.p.rapidapi.com/news',
+const createRequest = (url) => ({ url, headers: cryptoNewsHeaders });
+
+export const cryptoNewsApi = createApi({
+  reducerPath: "cryptoNewsApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptoNews: builder.query({
+      query: ({newsCategoy, count}) => createRequest(`/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`),
+    }),
+  }),
+});
+
+export const {useCryptoNewsQuery} = cryptoNewsApi;
