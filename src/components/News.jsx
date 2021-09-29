7fb8618874233f6936cd3a693f5677aa2e91560e
Paul Gonzalez <robertgonzalez018@gmail.com>
2021-09-29 12:17:44 -0700

    Added: added the cryptoNewsApi to the redux store

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
-import { Select, Typography, Row, Col, Avatar, Card } from 'antd';
-import moment from 'moment'
+import React from "react";
+import { Select, Typography, Row, Col, Avatar, Card } from "antd";
+import moment from "moment";
 
-const News = () => {
-  return (
-    <div>
-      News
-    </div>
-  )
-}
+import { useGetCryptoNewsQuery } from "../services/cryptoNewsApi";
+const { Text, Title } = Typography;
+const { Option } = Select;
 
-export default News
+const News = ({ simplified }) => {
+  const { data: cryptoNews } = useGetCryptoNewsQuery({ newsCategory: "Cryptocurrency",count: simplified ? 10 : 100});
+
+  if(!cryptoNews) return "Loading..."
+
+  return <div>
+    
+  </div>;
+};
+
+export default News;
--- src/services/cryptoNewsApi.js
+++ src/services/cryptoNewsApi.js
@@ -1,12 +1,12 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const cryptoNewsHeaders =  {
-  'x-bingapis-sdk': 'true',
-  'x-rapidapi-host': 'bing-news-search1.p.rapidapi.com',
-  'x-rapidapi-key': 'cbd49c280amsh357e0f10bd1fed8p1c7abajsnb602d80b00a7'
-}
+const cryptoNewsHeaders = {
+  "x-bingapis-sdk": "true",
+  "x-rapidapi-host": "bing-news-search1.p.rapidapi.com",
+  "x-rapidapi-key": "cbd49c280amsh357e0f10bd1fed8p1c7abajsnb602d80b00a7",
+};
 
-const baseUrl = 'https://bing-news-search1.p.rapidapi.com/news',
+const baseUrl = "https://bing-news-search1.p.rapidapi.com";
 const createRequest = (url) => ({ url, headers: cryptoNewsHeaders });
 
 export const cryptoNewsApi = createApi({
@@ -14,9 +14,12 @@ export const cryptoNewsApi = createApi({
   baseQuery: fetchBaseQuery({ baseUrl }),
   endpoints: (builder) => ({
     getCryptoNews: builder.query({
-      query: ({newsCategoy, count}) => createRequest(`/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`),
+      query: ({ newsCategory, count }) =>
+        createRequest(
+          `/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`
+        ),
     }),
   }),
 });
 
-export const {useCryptoNewsQuery} = cryptoNewsApi;
+export const { useGetCryptoNewsQuery } = cryptoNewsApi;
